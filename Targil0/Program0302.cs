c473a2620044f17a732df8259e74630ea4f88d03
HADAS HELLER <Handesaim3A9@mbjcomp.org.il>
2021-10-17 13:11:10 +0300

    adding welcom function

---
+++ Targil0/Program0302.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            welcom0302();
+            welcom8721();
+            Console.ReadKey();
+
+        }
+        static partial void welcom8721();
+
+        private static void welcom0302()
+        {
+            Console.WriteLine("enter your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("welcom {0}", userName);
+        }
+    }
+}
