c0d72e8b5983e7583fb22c465dd9a8759937e458
HADAS HELLER <Handesaim3A9@mbjcomp.org.il>
2021-10-18 15:21:51 +0300

    adding enum in IDAL.DO namespace

---
+++ DAL/enum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace IDAL.DO
+{
+    enum weightCategories { light , medium, heavy}
+    enum quadocopterState { empty, maintenance , Shipping }
+
+    enum priority { usual ,rapid, emergency }
+
+}
