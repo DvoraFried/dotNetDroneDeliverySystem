e90d06512b66c3aae7b6057f9ee5ca33da7aea9f
HADAS HELLER <Handesaim3A9@mbjcomp.org.il>
2021-10-19 17:44:41 +0300

    updaiting

--- DAL/dalObject.cs
+++ DAL/dalObject.cs
@@ -7,10 +7,25 @@ using IDAL.DO;
 
 namespace DalObject
 {
-    public class DalObject
+    public class DataSource
     {
-        static internal baseStation
+        static internal quadocopter[] myQuadocopters = new quadocopter[10];
+        static internal baseStation[] myBaseStations = new baseStation[5];
+        static internal customer[] myCustomers = new customer[100];
+        static internal package[] myPackages = new package[1000];
+        internal class Config
+        {
+            static internal int quadocoptersIndex=0;
+            static internal int baseStationsIndex = 0;
+            static internal int customersIndex = 0;
+            static internal int packagesIndex = 0;
+            static internal int idNumber;
+        }
 
+        static void Initialize()
+        {
+
+        }
     }
 
 }
