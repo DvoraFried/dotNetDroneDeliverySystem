51f7dd9e5f4c2a6931dec915c478af7464b5b37e
hadas heller <hadmirhel@gmail.com>
2021-10-21 22:13:48 +0300

    a

--- DAL/enum.cs
+++ DAL/enum.cs
@@ -2,8 +2,8 @@
 
 namespace IDAL.DO
 {
-    public enum weightCategories { light , medium, heavy}
+    public enum WeightCategories { light , medium, heavy}
     public enum DroneStatuses { empty, maintenance , Shipping }
-    public enum priorities { usual ,rapid, emergency }
+    public enum Priorities { usual ,rapid, emergency }
 
 }
