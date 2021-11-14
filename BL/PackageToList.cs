b9507b40a82c50e1657a6aae3912698a7ab4337a
Dvora Fried <013508520S@gmail.com>
2021-11-14 14:05:28 +0200

    up

---
+++ BL/PackageToList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    class PackageToList
+    {
+        public int Id { get; set; }
+
+        public string SenderName { get; set; }
+        public string UstomerReceivesName { get; set; }
+        public WeightCategory weight { get; set; }
+        public priority priority { get; set; }
+    }
+}
