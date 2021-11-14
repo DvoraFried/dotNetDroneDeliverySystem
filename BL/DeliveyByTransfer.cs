fe958a10a2343faf3d6e8cf22dd8845d46c3e096
הדס הלר <Handesaim3A9@mbjcomp.org.il>
2021-11-14 14:03:32 +0200

    up!

---
+++ BL/DeliveyByTransfer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    class DeliveyByTransfer
+    {
+        int Id { get; set; }
+        EnumBL.WeightCategoriesBL Weight { get; set; }
+        EnumBL.PrioritiesBL Priority { get; set; }
+        bool DeliveryStatus { get; set; }
+        Position CollectionLocation { get; set; }
+        Position DeliveryDestinationLocation { get; set; }
+
+        //Distance TransportDistance { get; set; }//creat a function that calculate a distance
+    }
+}
