b7ab560826f7ba49fc2338e15e59bb356e938a42
Dvora Fried <013508520S@gmail.com>
2021-11-14 14:12:08 +0200

    take that

---
+++ BL/DroneToList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    class DroneToList
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        EnumBL.WeightCategoriesBL Weight { get; set; }
+        //Battery status
+
+    }
+}
--- BL/PackageToList.cs
+++ BL/PackageToList.cs
@@ -9,10 +9,10 @@ namespace IBL.BO
     class PackageToList
     {
         public int Id { get; set; }
-
         public string SenderName { get; set; }
         public string UstomerReceivesName { get; set; }
-        public WeightCategory weight { get; set; }
-        public priority priority { get; set; }
+        EnumBL.WeightCategoriesBL weight { get; set; }
+        EnumBL.PrioritiesBL priority { get; set; }
+        EnumBL.DroneStatusesBL PackageStatus { get; set; }
     }
 }
