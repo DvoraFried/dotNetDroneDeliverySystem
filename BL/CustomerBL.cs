30ca895dafaf1ec1b9d4083767a28fca8eb7bf11
הדס הלר <Handesaim3A9@mbjcomp.org.il>
2021-11-15 17:59:03 +0200

    up

--- BL/CustomerBL.cs
+++ BL/CustomerBL.cs
@@ -8,10 +8,24 @@ namespace IBL.BO
 {
     public class CustomerBL
     {
-        public int IdBL { get; set; }
+        private int IdBL;
+        public void setIdBL(int idC) 
+        {
+            IEnumerable<CustomerBL> customers = new List<CustomerBL>(); //  במקום זה -> צריך למשוך מהדאטא את רשימת המשתמשים
+            foreach (CustomerBL customer in customers)
+            {
+                if (customer.getIdBL() == idC) {
+                    throw new ArgumentException("~ The ID number already exists in the system ~");
+                }
+            }
+            if(idC < 99999999 || idC > 999999999) {
+                throw new ArgumentException("~ An ID number should contain exactly 9 digits ~");
+            }
+            IdBL = idC;
+        }
+        public int getIdBL() { return IdBL; }
         public string NameBL { get; set; }
         public string PhoneBL { get; set; }
-        public double LongitudeBL { get; set; }
-        public double LatitudeBL { get; set; }
+        public Position position { get; set; }
     }
 }
--- BL/ParcelBL.cs
+++ BL/ParcelBL.cs
@@ -9,12 +9,37 @@ namespace IBL.BO
 {
     public class ParcelBL
     {
-        public int IdBL { get; set; }
-        public int SenderIdBL { get; set; }
-        public int TargetIdBL { get; set; }
+        private int IdBL;
+        public void setIdBL(int idP)
+        {
+            IEnumerable<ParcelBL> parcels = new List<ParcelBL>(); //  במקום זה -> צריך למשוך מהדאטא את רשימת התחנות
+            foreach (ParcelBL parcel in parcels)
+            {
+                if (parcel.getIdBL() == idP)
+                {
+                    throw new ArgumentException("~ The ID number already exists in the system ~");
+                }
+            }
+            IdBL = idP;
+        }
+        public int getIdBL() { return IdBL; }
+        private int SenderIdBL;
+        public void setSenderIdBL(int idS)
+        {
+            //לשלוף את רשימת הלקוחות ולעבור עליה (פיינד) - אם אין לקוח עם מספר מזהה כמו שהוזן לשלוח חריגה
+            SenderIdBL = idS;
+        }
+        public int getSenderIdBL() { return SenderIdBL; }
+        private int TargetIdBL;
+        public void setTargetIdBL(int idT)
+        {
+            //לשלוף את רשימת הלקוחות ולעבור עליה (פיינד) - אם אין לקוח עם מספר מזהה כמו שהוזן לשלוח חריגה
+            TargetIdBL = idT;
+        }
+        public int getTargetIdBL() { return TargetIdBL; }
         public WeightCategoriesBL Weight { get; set; }
         public PrioritiesBL Priority { get; set; }
-        public int DroneIdBL { get; set; }
+        public DroneBL DroneIdBL { get; set; }
         public DateTime RequestedBL { get; set; }//יצירת חבילה למשלוח
         public DateTime ScheduledBL { get; set; }//שיוך חבילה לרחפן
         public DateTime PickUpBL { get; set; }//איסוף חבילה מלקוח
