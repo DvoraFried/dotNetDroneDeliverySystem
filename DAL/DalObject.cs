0f305551a1694350b5fc3d4ae74584cd1159e5ec
Dvora Fried <013508520S@gmail.com>
2021-12-21 19:40:34 +0200

    commit all

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -196,17 +196,9 @@ namespace DalObject
         {
             DataSource.MyParcels[DataSource.MyParcels.IndexOf(DataSource.MyParcels.First(p => p.Id == DALP.Id))] = DALP;
         }
-
         public void DeleteObjFromDroneCharges(int id)
         {
             throw new NotImplementedException();
-        }
-        //=============================================
-        //remove item frm list by ID
-        //=============================================
-        public void RemoveCustomerById(int idCustomer)
-        {
-            DataSource.MyCustomers.RemoveAll(c => c.Id == idCustomer);
         }
     }
     
