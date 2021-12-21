fd94f2b0f85d92703c8d122de45c14a94165b931
הדס הלר <Handesaim3A9@mbjcomp.org.il>
2021-12-21 18:50:40 +0200

    add remove customer from list function

--- BL/RemoveItemsFromLists.cs
+++ BL/RemoveItemsFromLists.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BO.Exceptions;
+using DO;
 
 namespace BL
 {
     public partial class BL : BlApi.IBL
     {
-
+        public void RemoveCustomerById(int idCustomer)
+        {
+            if (DalObj.returnCustomerArray().ToList().Any(c => c.Id == idCustomer))  { throw new ObjectDoesntExistsInListException("customer"); }
+            DalObj.returnCustomerArray().ToList().RemoveAll(c => c.Id == idCustomer);
+        }
     }
 }
