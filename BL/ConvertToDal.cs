51b09a750d359d16b92754f57457de97b91c52a4
DvoraFried <013508520S@gmail.com>
2022-01-10 00:34:06 +0200

    fix is active

--- BL/ConvertToDal.cs
+++ BL/ConvertToDal.cs
@@ -11,55 +11,56 @@ namespace BO
     {
         public static StationDAL ConvertToStationDal(StationBL stationBl)
         {
-        StationDAL stationDal = new StationDAL();
-        stationDal.Id = stationBl.GetIdBL();
-        stationDal.Name = stationBl.NameBL;
-        stationDal.EmptyChargeSlots = stationBl.ChargeSlotsBL;
-        stationDal.Longitude = stationBl.Position.Longitude;
-        stationDal.Latitude = stationBl.Position.Latitude;
-        return stationDal;           
+            StationDAL stationDal = new StationDAL();
+            stationDal.Id = stationBl.GetIdBL();
+            stationDal.Name = stationBl.NameBL;
+            stationDal.EmptyChargeSlots = stationBl.ChargeSlotsBL;
+            stationDal.Longitude = stationBl.Position.Longitude;
+            stationDal.Latitude = stationBl.Position.Latitude;
+            return stationDal;           
         }
         public static DroneDAL ConvertToDroneDal(DroneBL droneBl)
         {
-        DroneDAL droneDal = new DroneDAL();
-        droneDal.Id = droneBl.getIdBL();
-        droneDal.Model = droneBl.ModelBL;
-        droneDal.MaxWeight = (WeightCategories)(int)droneBl.MaxWeight;
-        return droneDal;
+            DroneDAL droneDal = new DroneDAL();
+            droneDal.Id = droneBl.getIdBL();
+            droneDal.Model = droneBl.ModelBL;
+            droneDal.MaxWeight = (WeightCategories)(int)droneBl.MaxWeight;
+            return droneDal;
         }
         public static CustomerDAL ConvertToCustomerDal(CustomerBL customerBl)
         {
-        CustomerDAL customerDal = new CustomerDAL();
-        customerDal.Id = customerBl.getIdBL();
-        customerDal.Name = customerBl.NameBL;
-        customerDal.Phone = customerBl.PhoneBL;
-        customerDal.Longitude = customerBl.Position.Longitude;
-        customerDal.Latitude = customerBl.Position.Latitude;
-        return customerDal;
+            CustomerDAL customerDal = new CustomerDAL();
+            customerDal.Id = customerBl.getIdBL();
+            customerDal.Name = customerBl.NameBL;
+            customerDal.Phone = customerBl.PhoneBL;
+            customerDal.Longitude = customerBl.Position.Longitude;
+            customerDal.Latitude = customerBl.Position.Latitude;
+            return customerDal;
         }
         public static ParcelDAL ConvertToParcelDal(ParcelBL parcelBl)
         {
-        ParcelDAL parcelDal = new ParcelDAL();
-        parcelDal.Id = parcelBl.IdBL;
-        parcelDal.SenderId = parcelBl.Sender.Id;
-        parcelDal.TargetId = parcelBl.Target.Id;
-        parcelDal.Weight= (WeightCategories)(int)parcelBl.Weight;
-        parcelDal.Priority = (Priorities)(int)parcelBl.Priority;
-        parcelDal.DroneId = parcelBl.DroneIdBL == null ? -1 : parcelBl.DroneIdBL.Id;
-        parcelDal.Requested = parcelBl.RequestedBL;
-        parcelDal.Scheduled = parcelBl.ScheduledBL;
-        parcelDal.PickUp = parcelBl.PickUpBL;
-        parcelDal.Delivered = parcelBl.DeliveredBL;
-        return parcelDal;
+            ParcelDAL parcelDal = new ParcelDAL();
+            parcelDal.Id = parcelBl.IdBL;
+            parcelDal.SenderId = parcelBl.Sender.Id;
+            parcelDal.TargetId = parcelBl.Target.Id;
+            parcelDal.Weight= (WeightCategories)(int)parcelBl.Weight;
+            parcelDal.Priority = (Priorities)(int)parcelBl.Priority;
+            parcelDal.DroneId = parcelBl.DroneIdBL == null ? -1 : parcelBl.DroneIdBL.Id;
+            parcelDal.Requested = parcelBl.RequestedBL;
+            parcelDal.Scheduled = parcelBl.ScheduledBL;
+            parcelDal.PickUp = parcelBl.PickUpBL;
+            parcelDal.Delivered = parcelBl.DeliveredBL;
+            parcelDal.isActive = parcelBl.isActive;
+            return parcelDal;
         }
 
         public static DroneChargeDAL ConvertToDroneChargeDal(DroneInChargeBL droneBl,int stationIdS)
         {
-        DroneChargeDAL droneChargeDal = new DroneChargeDAL();
-        droneChargeDal.DroneId = droneBl.Id;
-        droneChargeDal.StationId = stationIdS;
-        droneChargeDal.enterTime = droneBl.enterTime;
-        return droneChargeDal;
+            DroneChargeDAL droneChargeDal = new DroneChargeDAL();
+            droneChargeDal.DroneId = droneBl.Id;
+            droneChargeDal.StationId = stationIdS;
+            droneChargeDal.enterTime = droneBl.enterTime;
+            return droneChargeDal;
         }
     }
 }
