161094235ee7a9361dd9d982558b87eeaedcd3b7
DvoraFried <013508520S@gmail.com>
2022-02-15 01:38:20 +0200

    imprive summarys at addBL

--- BL/Add Remove and Get/AddBL.cs
+++ BL/Add Remove and Get/AddBL.cs
@@ -19,28 +19,31 @@ namespace BL
         #region ADD FUNCTIONS
         /// <summary>
         /// the function gets pararmeters acording to the station constructor
-        /// create an bl obj and add is to the station list in dal. exception will be thrown in case of a station with the same id in 
-        /// station list.
+        /// create an BO station obj and add is to the station list in dal.
+        /// exception will be thrown in case of a station with the same id in station list.
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="name"></param>
-        /// <param name="longitude"></param>
-        /// <param name="latitude"></param>
-        /// <param name="chargeSlots"></param>
+        /// <param name="id">station id</param>
+        /// <param name="name">station name</param>
+        /// <param name="longitude">station longitude</param>
+        /// <param name="latitude">station latitude</param>
+        /// <param name="chargeSlots">station chargea lots</param>
         public void AddStation(int id, string name, double longitude, double latitude, int chargeSlots)
         {
             lock (DalObj)
             {
-                if (DalObj.GetStationList().Any(s => s.Id == id)) { throw new ObjectExistsInListException("Station"); };
+                if (DalObj.GetStationList().Any(s => s.Id == id)) {
+                    throw new ObjectExistsInListException("Station"); };
+
                 BO.Station station = new BO.Station(id, name, new Position(longitude, latitude), chargeSlots, DronesListBL);
                 DalObj.AddStationDAL(ConvertToDal.ConvertToStationDal(station));
             }
         }
+
         /// <summary>
-        /// the function gets pararmeters acording to the station constructor+id station
-        /// create an bl obj and add is to the drone list in dal, and add an intance of dronecharg
-        /// exception will be thrown in case of a station with the same drone id
-        /// drone list/ there is no station with the given id 
+        /// the function gets pararmeters acording to the drone constructor + id station
+        /// create an BO drone obj and add is to the drone list in dal as 'drone in maintenance',
+        /// and update the the station respectively.
+        /// The function refers to the state of adding an existing but inactive drone. 
         /// </summary>
         /// <param name="id"></param>
         /// <param name="model"></param>
@@ -50,28 +53,43 @@ namespace BL
         {
             lock (DalObj)
             {
-                if (DalObj.GetDroneList().Any(d => d.Id == id)) {
-                    throw new ObjectExistsInListException("drone"); };
-
                 if (!DalObj.GetStationList().Any(s => s.Id == stationId)) {
                     throw new ObjectDoesntExistsInListException("station"); };
+
+                BO.Drone drone = new BO.Drone(DalObj, id, model, maxWeight, DroneStatusesBL.maintenance, new Position(s.Longitude, s.Latitude), stationId);
+
+                if (DronesListBL.Any(d => d.Id == id)) {
+                    // If there is an active drone with the same ID:
+                    if (DronesListBL.First(d => d.Id == id).isActive) { 
+                        throw new ObjectExistsInListException("drone"); }
+                    // If there is an inactive drone with the same ID:
+                    DalObj.ReplaceDroneById(ConvertToDal.ConvertToDroneDal(drone));
+                    DronesListBL[DronesListBL.FindIndex(d => d.Id == id)] = drone;
+                }
+                else
+                {
+                    DalObj.AddDroneDAL(ConvertToDal.ConvertToDroneDal(drone));
+                    DronesListBL.Add(drone);
+                }
 
                 DO.Station s = DalObj.GetStationList().ToList().Find(d => d.Id == stationId);
                 s.DronesInCharging += 1;
                 s.EmptyChargeSlots -= 1;
                 
                 DalObj.ReplaceStationById(s);
-                BO.Drone drone = new BO.Drone(DalObj, id, model, maxWeight, DroneStatusesBL.maintenance, new Position(s.Longitude, s.Latitude), stationId);
-                DalObj.AddDroneDAL(ConvertToDal.ConvertToDroneDal(drone));
-                DronesListBL.Add(drone);
+                // add the drone to 'drones in charge' list:
                 DalObj.Charge(ConvertToDal.ConvertToDroneChargeDal(new BO.DroneInCharge(drone), s.Id));
-
+                
+                // dalegate to update the drones list in PL:
                 ActionUpdateList?.Invoke(true);
             }
         }
+
         /// <summary>
-        /// function gets param according to the customer -ctor .if the customer is not active he will turn to be active with the uptades info
-        /// if there is no customer with the id- function add customer to customer list,exception will be thrown in case of a active customer ation with the same id 
+        /// function gets param according to the customer-ctor,
+        /// if the customer is not active he will turn to be active with the uptades info,
+        /// if there is no customer with the id - the function add customer to customer list.
+        /// exception will be thrown in case of an active customer with the same id already exist
         /// </summary>
         /// <param name="id"></param>
         /// <param name="name"></param>
@@ -94,9 +112,10 @@ namespace BL
                 }
             }
         }
+
         /// <summary>
-        /// function create an bl obj with the give, params ,create an bl obj and ad it to parcel list
-        /// exception will be thrown in case of a parcel with the same  id
+        /// function create an BO parcel obj with the given params,
+        /// and aad it to parcel list.
         /// </summary>
         /// <param name="idSender"></param>
         /// <param name="idTarget"></param>
